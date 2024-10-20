d14b764fdf8be252804037c71ab33af923d0f411
HP <faisalyulianto26@gmail.com>
2024-10-20 23:59:45 +0700

    update be

--- stage-2/backend/index.ts
+++ stage-2/backend/index.ts
@@ -20,11 +20,11 @@ const app = express();
 const prisma = new PrismaClient();
 const port = process.env.PORT ;
 const corsOptions = {
-  origin:  'http://localhost:5173', 
+  origin:  'https://dumways-stage2-cp18.vercel.app/', 
   credentials: true, 
 };
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.header('Access-Control-Allow-Origin', 'https://dumways-stage2-cp18.vercel.app/');
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
